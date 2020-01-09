ec696d19694ae62dd284fec5622b26a439193e0b
Dan Willemsen <dwillemsen@google.com>
2020-01-08 22:33:03 -0800

    Fixup usage of lex/yacc in Android.mk files
    Add M4 environment variable for flex so it uses our prebuilt instead of
    the one in $PATH.
    
    Add dependencies on the prebuilt executables and data files for flex and
    bison.
    
    Use .KATI_IMPLICIT_OUTPUTS to specify that a single rule creates
    multiple files.
    
    src/mesa/program/... was standard enough to use the built-in lex
    support, so move program_lexer.l to the sources list.
    
    Switch from custom macros to using the common transform-y-to-c-or-cpp
    macro the build system uses for yacc. We can't just add them to the
    sources list because the filenames are different and the YACCFLAGS
    differ in the same module.
    
    Bug: 117561006
    Test: mmm external/mesa3d
    Change-Id: I615d85714409cb03221a2bebb0bdd5d25e7313d9
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- src/freedreno/Android.ir3.mk
+++ src/freedreno/Android.ir3.mk
@@ -75,10 +75,11 @@ ir3_nir_trig_deps := \
 ir3_parser_deps := \
 	$(MESA_TOP)/src/freedreno/ir3/ir3_parser.y
 
-$(intermediates)/ir3/ir3_lexer.c: $(ir3_lexer_deps)
+$(intermediates)/ir3/ir3_lexer.c: $(ir3_lexer_deps) $(LEX) $(M4)
 	@mkdir -p $(dir $@)
 	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
 	$(hide) $(MESA_LEX) -o $@ $<
+	$(hide) M4=$(M4) $(LEX) --nounistd -o$@ $<
 
 $(intermediates)/ir3/ir3_nir_imul.c: $(ir3_nir_imul_deps)
 	@mkdir -p $(dir $@)
@@ -88,15 +89,17 @@ $(intermediates)/ir3/ir3_nir_trig.c: $(ir3_nir_trig_deps)
 	@mkdir -p $(dir $@)
 	$(hide) $(MESA_PYTHON3) $< -p $(MESA_TOP)/src/compiler/nir > $@
 
-$(intermediates)/ir3/ir3_parser.c: $(ir3_parser_deps)
+$(intermediates)/ir3/ir3_parser.c: $(ir3_parser_deps) $(BISON) $(BISON_DATA) $(M4)
 	@mkdir -p $(dir $@)
 	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
 	$(hide) $(BISON) $< --name-prefix=ir3_yy --output=$@
+	$(hide) M4=$(M4) $(BISON) $< --name-prefix=ir3_yy --output=$@
 
-$(intermediates)/ir3/ir3_parser.h: $(ir3_parser_deps)
+$(intermediates)/ir3/ir3_parser.h: $(ir3_parser_deps) $(BISON) $(BISON_DATA) $(M4)
 	@mkdir -p $(dir $@)
 	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
 	$(hide) $(BISON) $< --name-prefix=ir3_yy --defines=$@
+	$(hide) M4=$(M4) $(BISON) $< --name-prefix=ir3_yy --defines=$@
 
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
