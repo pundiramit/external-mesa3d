c81529e8b72f2f05fb8820401ea0901254c4a47d
Amit Pundir <amit.pundir@linaro.org>
2020-12-17 02:27:40 +0530

    ANDROID: prebuilt-intermediates: Add bifrost prebuilts and build rules
    Add bifrost/* prebuilts along with aosp-gen-prebuilt.sh build
    rules to generate them and corresponding Android.mk changes
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- src/panfrost/Android.bifrost.mk
+++ src/panfrost/Android.bifrost.mk
@@ -41,6 +41,7 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
 
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 intermediates := $(call local-generated-sources-dir)
+prebuilt_intermediates := $(MESA_TOP)/prebuilt-intermediates
 
 LOCAL_GENERATED_SOURCES := \
 	$(intermediates)/bifrost_gen_disasm.c
@@ -48,9 +49,9 @@ LOCAL_GENERATED_SOURCES := \
 bifrost_gen_disasm_gen := $(LOCAL_PATH)/bifrost/gen_disasm.py
 bifrost_gen_disasm_deps := $(LOCAL_PATH)/bifrost/ISA.xml $(LOCAL_PATH)/bifrost/bifrost_isa.py
 
-$(intermediates)/bifrost_gen_disasm.c: $(bifrost_gen_disasm_deps)
+$(intermediates)/bifrost_gen_disasm.c: $(prebuilt_intermediates)/bifrost/bifrost_gen_disasm.c
 	@mkdir -p $(dir $@)
-	$(hide) $(MESA_PYTHON3) $(bifrost_gen_disasm_gen) $< > $@
+	@cp -f $< $@
 
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
@@ -61,6 +62,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libpanfrost_bifrost
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 intermediates := $(call local-generated-sources-dir)
+prebuilt_intermediates := $(MESA_TOP)/prebuilt-intermediates
 
 LOCAL_SRC_FILES := \
 	$(bifrost_FILES)
@@ -94,9 +96,16 @@ bifrost_nir_algebraic_gen := $(LOCAL_PATH)/bifrost/bifrost_nir_algebraic.py
 bifrost_nir_algebraic_deps := \
 	$(MESA_TOP)/src/compiler/nir/
 
-$(intermediates)/bifrost_nir_algebraic.c: $(bifrost_nir_algebraic_deps)
+$(intermediates)/bifrost_nir_algebraic.c: $(prebuilt_intermediates)/bifrost/bifrost_nir_algebraic.c
 	@mkdir -p $(dir $@)
-	$(hide) $(MESA_PYTHON3) $(bifrost_nir_algebraic_gen) -p $< > $@
+	@cp -f $< $@
+
+bi_generated_pack_gen := $(LOCAL_PATH)/bifrost/gen_pack.py
+bi_generated_pack_deps := $(LOCAL_PATH)/bifrost/ISA.xml
+
+$(intermediates)/bi_generated_pack.h: $(prebuilt_intermediates)/bifrost/bi_generated_pack.h
+	@mkdir -p $(dir $@)
+	@cp -f $< $@
 
 bi_builder_h_gen := $(LOCAL_PATH)/bifrost/bi_builder.h.py
 bi_builder_h_deps := $(LOCAL_PATH)/bifrost/ISA.xml $(LOCAL_PATH)/bifrost/bifrost_isa.py
