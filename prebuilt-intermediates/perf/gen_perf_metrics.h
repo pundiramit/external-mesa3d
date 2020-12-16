johnstultz-work/mesa

prebuild: Add generated prebuilt intermediates
DO NOT REBASE, REGENERATE INSTEAD.

This is the result of running the prebuilt generation script

Change-Id: I9df31a314d369bbc58a9ddadfb26b0ea1dac33ff
Signed-off-by: John Stultz <john.stultz@linaro.org>

prebuilt-intermediates/perf/gen_perf_metrics.h
@@ -1,0 +1,42 @@
+/* Autogenerated file, DO NOT EDIT manually! generated by gen_perf.py
+ *
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+struct gen_perf;
+
+void gen_oa_register_queries_hsw(struct gen_perf *perf);
+void gen_oa_register_queries_bdw(struct gen_perf *perf);
+void gen_oa_register_queries_chv(struct gen_perf *perf);
+void gen_oa_register_queries_sklgt2(struct gen_perf *perf);
+void gen_oa_register_queries_sklgt3(struct gen_perf *perf);
+void gen_oa_register_queries_sklgt4(struct gen_perf *perf);
+void gen_oa_register_queries_bxt(struct gen_perf *perf);
+void gen_oa_register_queries_kblgt2(struct gen_perf *perf);
+void gen_oa_register_queries_kblgt3(struct gen_perf *perf);
+void gen_oa_register_queries_glk(struct gen_perf *perf);
+void gen_oa_register_queries_cflgt2(struct gen_perf *perf);
+void gen_oa_register_queries_cflgt3(struct gen_perf *perf);
+void gen_oa_register_queries_cnl(struct gen_perf *perf);
+void gen_oa_register_queries_icl(struct gen_perf *perf);

pundiramit/external-mesa3d

prebuilt-intermediates/perf/gen_perf_metrics.h
@@ -1,0 +1,44 @@
+/* Autogenerated file, DO NOT EDIT manually! generated by gen_perf.py
+ *
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+struct gen_perf_config;
+
+void gen_oa_register_queries_hsw(struct gen_perf_config *perf);
+void gen_oa_register_queries_bdw(struct gen_perf_config *perf);
+void gen_oa_register_queries_chv(struct gen_perf_config *perf);
+void gen_oa_register_queries_sklgt2(struct gen_perf_config *perf);
+void gen_oa_register_queries_sklgt3(struct gen_perf_config *perf);
+void gen_oa_register_queries_sklgt4(struct gen_perf_config *perf);
+void gen_oa_register_queries_bxt(struct gen_perf_config *perf);
+void gen_oa_register_queries_kblgt2(struct gen_perf_config *perf);
+void gen_oa_register_queries_kblgt3(struct gen_perf_config *perf);
+void gen_oa_register_queries_glk(struct gen_perf_config *perf);
+void gen_oa_register_queries_cflgt2(struct gen_perf_config *perf);
+void gen_oa_register_queries_cflgt3(struct gen_perf_config *perf);
+void gen_oa_register_queries_cnl(struct gen_perf_config *perf);
+void gen_oa_register_queries_icl(struct gen_perf_config *perf);
+void gen_oa_register_queries_lkf(struct gen_perf_config *perf);
+void gen_oa_register_queries_tgl(struct gen_perf_config *perf);